7e757b272f80970b6d14e097cdad031b012efe71
Gema <gemarais55@gmail.com>
2026-05-10 21:45:46 +0700

    fix: change api url from localhost to railway production

--- sicata-backend/config/cors.php
+++ sicata-backend/config/cors.php
@@ -3,12 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'auth/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [
-        'http://127.0.0.1:5500',
-        'http://127.0.0.1:5501',
-        'http://localhost:5500',
-        'http://localhost:5501',
-    ],
+    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
